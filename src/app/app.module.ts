6eaa9f623f5ddc55c2f7bbc38395639316c17a2f
Maxime TFT <mkubik@tftlabs.com>
2021-09-08 10:51:18 +0200

    add account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
