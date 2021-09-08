cffd10c7f306219b6188498e4875957ec304f5ff
Maxime TFT <mkubik@tftlabs.com>
2021-09-08 10:47:49 +0200

    init angular app and add supabase service

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  supabaseUrl: "https://vbtbeukfuajfpmbbtxgp.supabase.co",
+  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTA4OTQzNywiZXhwIjoxOTQ2NjY1NDM3fQ.l0K0eFEox6XqZnOe-HF70x2RXn2s6sljJ_6jCn0q7Qc"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
